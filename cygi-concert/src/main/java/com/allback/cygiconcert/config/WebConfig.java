b5b8ab36fe59958dcfd89eef2275bb6651b878fa
Juna Choi <jn307742@gmail.com>
2023-08-29 16:59:34 +0900

    fix: WebConfig.java

--- cygi-concert/src/main/java/com/allback/cygiconcert/config/WebConfig.java
+++ cygi-concert/src/main/java/com/allback/cygiconcert/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.allback.cygiconcert.config;
 
 //import com.allback.cygiconcert.config.interceptor.KafkaInterceptor;
-
+import com.allback.cygiconcert.config.interceptor.QueueInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
