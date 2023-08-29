195da05dbef13f24107aed1641728d11cb23b38e
Juna Choi <jn307742@gmail.com>
2023-08-29 17:25:02 +0900

    fix: WebConfig.java

--- cygi-concert/src/main/java/com/allback/cygiconcert/config/WebConfig.java
+++ cygi-concert/src/main/java/com/allback/cygiconcert/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
